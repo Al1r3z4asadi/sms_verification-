8bd2ac36190375c9ae33059ee87451e6362d36a4
alirez asadi <ali.s.asadi98@gmail.com>
2020-11-04 15:22:25 +0330

    app updated

--- app.py
+++ app.py
@@ -5,3 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 def main_page():
     return 'Hello World' 
+
+
+@app.route('/v1/process')
+def process():
+    pass
+
+
+def send_sms():
+    pass 
+
+
+def check_serial():
+    pass 
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0' , 5000)
