3b5df51bf38c1087b8cc79c22453870b7e14cfe9
alirez asadi <ali.s.asadi98@gmail.com>
2020-11-08 12:43:49 +0330

    added sending sms

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-from flask import Flask
+from flask import Flask , jsonify , request 
+import requests
+
 app = Flask(__name__)
 
 
-@app.route('/')
-def main_page():
-    return 'Hello World' 
 
 
-@app.route('/v1/process')
+def send_sms(receptor , message):
+    
+    
+    my_api_key = '713831722B743341486E354A444D5466367A742F414758356457794B675172794955794364453836724A733D'
+    url = f'https://api.kavenegar.com/v1/{my_api_key}/sms/send.json'
+    data = {
+        "receptor" : receptor,
+        "message" : "we got your massage {}".format(receptor)
+                }   
+    res = requests.post(url , data=data)
+
+
+@app.route('/v1/process',methods=['POST'])
 def process():
-    pass
+    # import pdb;pdb.set_trace()
+    form = request.form
+    sender = form['from']
+    message = form['message']
+    send_sms(sender , message)
+    return form
 
-
-def send_sms():
-    pass 
 
 
 def check_serial():
@@ -21,4 +34,4 @@ def check_serial():
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0' , 5000)
+    app.run('0.0.0.0' , 5000 , debug = True)
